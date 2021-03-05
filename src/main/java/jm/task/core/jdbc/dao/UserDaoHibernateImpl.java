6520bd95430da85a12406a568fb5f16d25c36718
serkali88 <serkali88@gmail.com>
2021-03-05 21:49:01 +0400

    Java pre-project. Практическая задача 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,91 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
+    private final String tableName = "users";
 
+    public UserDaoHibernateImpl() {
     }
 
-
+    //создание таблицы юзеров
     @Override
     public void createUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.createSQLQuery("CREATE TABLE IF NOT EXISTS " + tableName +
+                "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY , name VARCHAR(100) NOT NULL, " +
+                "lastName VARCHAR(100) NOT NULL, age MEDIUMINT NOT NULL) DEFAULT CHARSET=utf8")
+                .executeUpdate();
+        tx1.commit();
+        session.close();
+        System.out.println("Таблица создана");
     }
 
+    //удаление таблицы юзеров
     @Override
     public void dropUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS " + tableName).executeUpdate();
+        tx1.commit();
+        session.close();
+        System.out.println("Таблица удалена");
     }
 
+    //добавление юзеров
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        User user = new User();
+        user.setName(name);
+        user.setLastName(lastName);
+        user.setAge(age);
+        Session session = Util.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.save(user);
+        tx1.commit();
+        session.close();
+        System.out.println("Пользователь добавлен");
     }
 
+    //удаление юзера по id
     @Override
     public void removeUserById(long id) {
-
+        Session session = Util.getSessionFactory().openSession();
+        User user = (User) session.get(User.class, id);
+        Transaction tx1 = session.beginTransaction();
+        session.delete(user);
+        tx1.commit();
+        session.close();
+        System.out.println("Пользователь удален");
     }
 
+    //получение юзеров
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = Util.getSessionFactory().openSession();
+        String sql = "SELECT * FROM users";
+        List<User> list = session.createSQLQuery(sql).addEntity(User.class).list();
+        session.close();
+        return list;
+
     }
 
+
+    //удаление всех юзеров
     @Override
     public void cleanUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.createSQLQuery("DELETE FROM " + tableName).executeUpdate();
+        tx1.commit();
+        session.close();
+        System.out.println("Пользователи удалены");
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
@@ -8,38 +10,38 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl  implements UserService {
-    private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+  UserDaoHibernateImpl dao = new UserDaoHibernateImpl();
 
 
     //создание таблицы
     public void createUsersTable() {
-        userDaoJDBC.createUsersTable();
+        dao.createUsersTable();
     }
 
     //удаление таблицы
     public void dropUsersTable() {
-        userDaoJDBC.dropUsersTable();
+      dao.dropUsersTable();
 
     }
 
     // добавление USER  в таблицу
     public void saveUser(String name, String lastName, byte age) {
-        userDaoJDBC.saveUser(name,lastName,age);
+        dao.saveUser(name,lastName,age);
 
 
     }
 
     // удаление User из таблицы по id
     public void removeUserById(long id) {
-        userDaoJDBC.removeUserById(id);
+        dao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return userDaoJDBC.getAllUsers();
+        return dao.getAllUsers();
     }
 
     // очистка содержания таблицы
     public void cleanUsersTable() {
-        userDaoJDBC.cleanUsersTable();
+        dao.cleanUsersTable();
     }
 }
